8916a55f5e2eac02ca260a265b6a42f00dda9493
Dongwon Kim <freshtuna@kakao.com>
2022-08-14 17:33:53 +0900

    로그인 페이지 프로토타입 구현

---
+++ src/pages/SigninPage.vue
@@ -0,0 +1,18 @@
+<template>
+<v-app>
+    <v-main style="min-width: 1000px">
+        <Form/>
+    </v-main>
+</v-app>
+</template>
+
+<script>
+import Form from '@/components/signin-page/form.vue'
+
+export default {
+  name: 'MainPage',
+  components: {Form}
+};
+</script>
+<style>
+</style>
