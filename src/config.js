bee5c86f227720f2a5abe9221afdd8db0288aee8
Dongwon Kim <freshtuna@kakao.com>
2022-08-14 17:31:56 +0900

    환경변수 기능 추가

---
+++ src/config.js
@@ -0,0 +1,18 @@
+/**
+ * This file contains configurations
+ */
+
+const CONFIG = {
+  /**
+   * API addresses
+   */
+  MAIN_API_ROOT: process.env.VUE_APP_MAIN_API_ROOT,
+
+
+  /**
+   * Auth token prefix
+   */
+  MAIN_API_TOKEN_PREFIX: process.env.VUE_APP_MAIN_API_TOKEN_PREFIX
+}
+
+export default CONFIG;
